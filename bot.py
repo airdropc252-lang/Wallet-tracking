455e581e5b0c5ac2e344dfd776235d4ab896db94
airdropc252-lang <airdropc252@gmail.com>
2026-06-04 10:42:35 +0700

    update token baru

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN = "8807718291:AAH5Ytj7tHmvAQgepKdjacR-Smn_73sxngY"
+TELEGRAM_TOKEN = "8807718291:AAHMFi4-wwsfbICu49XkIA4fHP5w4lE_cUk"
 TELEGRAM_CHAT_ID = "-1003734394227"
 HELIUS_API_KEY = "5ddc9539-90ae-419b-8ab1-fc1677cdb01e"
 
